42135c6e28128cb9be5a1281c30e5c0f7e304ecf
Eduarda <eduarda_tomaz@estudante.sesisenai.org.br>
2025-08-15 11:31:56 -0300

    add

--- create.php
+++ create.php
@@ -1,47 +1,39 @@
 <?php
-
 include 'db.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $preco = $_POST['preco'];
+    $quantidade = $_POST['quantidade'];
+    $id_usuario = 1; 
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
+    $sql = "INSERT INTO produtos (nome, descricao, preco, quantidade_estoque, id_usuario)
+            VALUES ('$nome', '$descricao', '$preco', '$quantidade', '$id_usuario')";
 
-    $sql = " INSERT INTO usuarios (name,email) VALUE ('$name','$email')";
-
-    if ($conn->query($sql) === true) {
-        echo "Novo registro criado com sucesso.";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: index.php");
     } else {
-        echo "Erro " . $sql . '<br>' . $conn->error;
+        echo "Erro: " . $conn->error;
     }
-    
 }
-
 ?>
-
-
-<html lang="en">
-
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create</title>
+    <title>Adicionar Produto</title>
+    
 </head>
 <body>
-
-    <form method="POST" action="create.php">
-
-        <label for="name">Nome:</label>
-        <input type="text" name="name" required>
-
-        <label for="email">Email:</label>
-        <input type="email" name="email" required>
-
-        <input type="submit" value="Adicionar">
-
+    <h1>Adicionar Produto</h1>
+    <form method="POST">
+        Nome: <input type="text" name="nome" required><br>
+        Descrição: <textarea name="descricao" required></textarea><br>
+        Preço: <input type="number" step="0.01" name="preco" required><br>
+        Quantidade: <input type="number" name="quantidade" required><br>
+        <button type="submit">Salvar</button>
     </form>
-
-    <a href="read.php">Ver registros.</a>
-
+    <a href="index.php">Voltar</a>
 </body>
 </html>
